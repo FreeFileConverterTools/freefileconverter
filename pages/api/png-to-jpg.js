b1c8737cb999d28191f229a001cd45a10c08e118
FreeFileConverterTools <parvesahmed0105@gmail.com>
2025-12-10 23:05:20 +0600

    Update png-to-jpg.js

--- pages/api/png-to-jpg.js
+++ pages/api/png-to-jpg.js
@@ -3,17 +3,18 @@ import fs from "fs";
 import sharp from "sharp";
 
 export const config = {
-  api: { bodyParser: false }
+  api: { bodyParser: false },
 };
 
 export default async function handler(req, res) {
   const form = new formidable.IncomingForm();
 
   form.parse(req, async (err, fields, files) => {
-    const file = fs.readFileSync(files.file.filepath);
-    const output = await sharp(file).jpeg().toBuffer();
+    const file = files.file;
+
+    const jpgBuffer = await sharp(file.filepath).jpeg().toBuffer();
 
     res.setHeader("Content-Type", "image/jpeg");
-    res.send(output);
+    res.send(jpgBuffer);
   });
-}
+      }
